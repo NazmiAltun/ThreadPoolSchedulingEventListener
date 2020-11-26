1766b24752d3ba012713b31dd5820212e355cc41
Nazmi Altun <nazmialtun@windowslive.com>
2020-11-26 10:16:26 +0800

    Clarification

--- FrameworkEventListener/ThreadPoolSchedulingEventListener.cs
+++ FrameworkEventListener/ThreadPoolSchedulingEventListener.cs
@@ -6,6 +6,8 @@ namespace FrameworkEventListener
     public class ThreadPoolSchedulingEventListener : EventListener
     {
         private const long ThreadPoolKeyword = 0x0002;
+        private const int EnqueueId = 30;
+        private const int DequeueId = 30;
         private static readonly Guid EventSourceGuid = Guid.Parse("8E9F5090-2D75-4d03-8A81-E5AFBF85DAF1");
 
         private readonly Action<string> _writer;
@@ -18,7 +20,12 @@ namespace FrameworkEventListener
 
         protected override void OnEventWritten(EventWrittenEventArgs eventData)
         {
-            _writer($"EventId :{eventData.EventId} Payload[0]={eventData.Payload[0]}");
+            _writer($"EventId={eventData.EventId} Event={GetEvent(eventData.EventId)} Payload[0]={eventData.Payload[0]}");
+        }
+
+        private string GetEvent(int id)
+        {
+            return id == EnqueueId ? "Enqueue" : "Dequeue";
         }
 
         private void OnEventSourceCreated(object sender, EventSourceCreatedEventArgs e)
